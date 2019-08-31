56735b42d88d49982195b75a0e117581ca1517db
peakercope <peakercope@gmail.com>
2019-08-31 12:35:32 +0300

    updated dependencies

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_URI,
