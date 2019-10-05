f27a0a6cb37d54d6cf40756436fd9c46b4ac7d61
peakercope <peakercope@gmail.com>
2019-10-05 16:23:02 +0300

    updated dependencies. removed react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from 'react-apollo';
 import client from '../apollo';
 import store from '../store';
+import Welcome from "./Welcome";
 
 const App = () => (
     <ApolloProvider client={client}>
         <Provider store={store}>
           <BrowserRouter>
-
+              <Switch>
+                  <Route path="/" component={Welcome} />
+              </Switch>
           </BrowserRouter>
         </Provider>
     </ApolloProvider>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
 
 function Welcome() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
